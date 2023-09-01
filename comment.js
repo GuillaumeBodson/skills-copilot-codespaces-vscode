b57098b7047f07dc51682b6a5e8ebb53a2557078
GuillaumeBodson <48444214+GuillaumeBodson@users.noreply.github.com>
2023-09-01 04:35:53 +0000

    Add comment.js file

---
+++ comment.js
@@ -0,0 +1,14 @@
+// create a web server
+// http://localhost:3000/comments
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// set the view engine to ejs
+app.set("view engine", "ejs");
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get("/comments", function (req, res) {
+  var comments = [
